4f440edd93c36f5421ac29509a214ea5f475969c
Roger Almeida <roger.eduardo@gmail.com>
2020-10-24 20:56:40 +1100

    :lipstick: Minimal voting form

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Form, Row } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <Container>
         <Row>
-          <Col>1 of 1</Col>
-        </Row>
+          <Col>
+            <Form>
+              <Form.Group>
+                <Form.Label>Cidade</Form.Label>
+                <Form.Control as="select">
+                  <option value="1">Caieiras</option>
+                  <option value="2">Francisco Morato</option>
+                  <option value="3">Franco da Rocha</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Vereador</Form.Label>
+                <Form.Control as="select">
+                  <option value="1">Fulano</option>
+                  <option value="2">Beltrano</option>
+                  <option value="3">Ciclano</option>
+                </Form.Control>
+              </Form.Group>
+              <Form.Group>
+                <Form.Label>Prefeito</Form.Label>
+                <Form.Control as="select">
+                  <option value="1">Candidato Honesto</option>
+                  <option value="2">Candidato Louco</option>
+                  <option value="3">Outro Candidato</option>
+                </Form.Control>
+              </Form.Group>
+            </Form>
+        </Col>
+      </Row>
       </Container>
     </div>
   );
